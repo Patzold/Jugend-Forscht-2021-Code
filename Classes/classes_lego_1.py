41c151d7efab1a710368f3951b36d613135ebf7f
Patzold <60754058+Patzold@users.noreply.github.com>
2020-12-28 10:18:14 +0100

    Class: Lego

--- Classes/classes_lego_1.py
+++ Classes/classes_lego_1.py
@@ -1,5 +1,5 @@
 import os
-os.chdir("FlexNet_Baseline")
+os.chdir("Classes")
 import random
 import matplotlib.pyplot as plt
 import datetime
@@ -26,14 +26,46 @@ torch.backends.cudnn.deterministic = True
 
 base_dir = "C:/Datasets/PJF-30/data/"
 save_dir = "C:/Datasets/PJF-30/safe/"
+categorys = [8, 9, 10, 11, 12, 13]
 
-pickle_in = open(save_dir + "intm_3_raw.pickle","rb")
-train = pickle.load(pickle_in)
-pickle_in = open(save_dir + "intm_3t_raw.pickle","rb")
-test = pickle.load(pickle_in)
+train = []
+test = []
 
+if False:
+    for dir in tqdm(categorys):
+        path = base_dir + str(dir) + "/comp/"
+        for num, img in enumerate(os.listdir(path)):
+            try:
+                img_in = cv2.imread((path + "/" + img), cv2.IMREAD_COLOR)
+                img_resz = cv2.resize(img_in, (224, 224))
+                if num < 2000:
+                    train.append([img_resz, (dir - 8)])
+                else:
+                    test.append([img_resz, (dir - 8)])
+            except Exception as e: pass
+        print(len(train), len(test))
+
+    pickle_out = open((save_dir + "classes_lego.pickle"),"wb")
+    pickle.dump(train, pickle_out)
+    pickle_out.close()
+    pickle_out = open((save_dir + "classes_legot.pickle"),"wb")
+    pickle.dump(test, pickle_out)
+    pickle_out.close()
+else:
+    pickle_in = open(save_dir + "classes_lego.pickle","rb")
+    train = pickle.load(pickle_in)
+    pickle_in = open(save_dir + "classes_legot.pickle","rb")
+    test = pickle.load(pickle_in)
 l = len(train)
+check = [0, 0, 0, 0, 0, 0]
+for i in range(l):
+    check[train[i][1]] += 1
+print(check)
 lt = len(test)
+check = [0, 0, 0, 0, 0, 0]
+for i in range(lt):
+    check[test[i][1]] += 1
+print(check)
 print(len(train), len(test))
 random.shuffle(train)
 random.shuffle(test)
@@ -46,19 +78,12 @@ for features, lables in train:
 for features, lables in test:
     Xt.append(features)
     yt.append(lables)
-temp = np.array(y)
-print(np.max(temp))
-X = np.array(X, dtype=np.float32)
+
+X = np.array(X, dtype=np.float32) / 255
 y = np.array(y, dtype=np.int64)
-Xt = np.array(Xt, dtype=np.float32)
+Xt = np.array(Xt, dtype=np.float32) / 255
 yt = np.array(yt, dtype=np.int64)
 print(np.max(X[0]), np.max(Xt[0]))
-
-# for i in range(0, len(X), 100):
-#     print(X[i].tolist().count(1), X[i], y[i])
-#     input()
-
-# quit()
 
 X = torch.from_numpy(X)
 y = torch.from_numpy(y)
@@ -70,16 +95,10 @@ yt = torch.from_numpy(yt)
 Xt.to(torch.float32)
 yt.to(torch.int64)
 print(Xt.dtype, yt.dtype)
-print(y[10:], yt[:10])
-print(X[10:], Xt[:10])
-
-check = [0, 0]
+print(y[:10], yt[:10])
+check = [0, 0, 0, 0, 0, 0]
 for i in range(l):
-        check[y[i].numpy()] += 1
-print(check)
-check = [0, 0]
-for i in range(lt):
-        check[yt[i].numpy()] += 1
+    check[y[i].numpy()] += 1
 print(check)
 
 train_on_gpu = torch.cuda.is_available()
@@ -97,26 +116,49 @@ else:
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(6, 192)
-        self.fc2 = nn.Linear(192, 64)
-        self.fc3 = nn.Linear(64, 2)
+        self.conv1 = nn.Conv2d(3, 12, 2)
+        self.conv2 = nn.Conv2d(12, 24, 2)
+        self.dropout = nn.Dropout(0.5)
+        
+        x = torch.randn(224,224,3).view(-1,3,224,224)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 200) #flattening.
+        self.fc2 = nn.Linear(200, 100)
+        self.fc3 = nn.Linear(100, 6)
+
+    def convs(self, x):
+            c1 = self.conv1(x)
+            relu1 = F.relu(c1)
+            pool1 = F.max_pool2d(relu1, (2, 2))
+            c2 = self.conv2(pool1)
+            relu2 = F.relu(c2)
+            pool2 = F.max_pool2d(relu2, (2, 2))
+            
+            if self._to_linear is None:
+                self._to_linear = pool2[0].shape[0]*pool2[0].shape[1]*pool2[0].shape[2]
+                print("to linear: ", self._to_linear)
+            return pool2
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
         x = self.fc3(x)
         return x
 
 net = Net()
+# torch.save(net, save_dir + "smple_conv_model.pt")
 net.to(device)
 print(net)
 
-optimizer = optim.Adam(net.parameters(), lr=0.0001)
+optimizer = optim.Adam(net.parameters(), lr=0.001) #optim.SGD(net.parameters(), lr=0.01)
 loss_function = nn.CrossEntropyLoss()
 
 BATCH_SIZE = 100
 EPOCHS = 100
-
 train_log = []
 eval_size = int(len(X)*0.1)
 eval_X = X[:eval_size]
@@ -127,7 +169,7 @@ train_data = []
 log = []
 valid_loss_min = np.Inf # track change in validation loss
 valid_acc_min = 0
-
+print(y[:20], eval_y[:20], yt[:20])
 def evaluate():
     net.eval()
     correct = 0
@@ -135,7 +177,7 @@ def evaluate():
     with torch.no_grad():
         for i in tqdm(range(len(eval_X))):
             real_class = eval_y[i].to(device)
-            net_out = net(eval_X[i].view(-1, 6).to(device))[0]  # returns a list
+            net_out = net(eval_X[i].view(-1, 3, 224, 224).to(device))[0]  # returns a list
             predicted_class = torch.argmax(net_out)
             # print(real_class, net_out, predicted_class)
             # input()
@@ -146,20 +188,19 @@ def evaluate():
     in_sample_acc = round(correct/total, 3)
     correct = 0
     total = 0
-    # Xta = Xt[:1500]
-    # yta = yt[:1500]
-    check = [0, 0]
+    Xta = Xt[:1500]
+    yta = yt[:1500]
     with torch.no_grad():
-        for i in tqdm(range(len(Xt))):
-            real_class = yt[i].to(device)
-            net_out = net(Xt[i].view(-1, 6).to(device))[0]  # returns a list
+        for i in tqdm(range(len(Xta))):
+            real_class = yta[i].to(device)
+            net_out = net(Xta[i].view(-1, 3, 224, 224).to(device))[0]  # returns a list
             predicted_class = torch.argmax(net_out)
+            # print(real_class, net_out, predicted_class)
+            # input()
             if predicted_class == real_class:
                 correct += 1
-                check[predicted_class.cpu().numpy()] += 1
             # else: cv2.imwrite(("D:/Datasets\stupid/test/i" + str(i) + ".jpg"), Xt[i].view(60, 60, 1).numpy())
             total += 1
-    print(check)
     out_of_sample_acc = round(correct/total, 3)
     return in_sample_acc, out_of_sample_acc
 
@@ -177,8 +218,8 @@ for epoch in range(EPOCHS):
         # Actual training
         net.zero_grad()
         optimizer.zero_grad()
-        outputs = net(batch_X.view(-1, 6))
-        # print(batch_X, batch_y, outputs)
+        outputs = net(batch_X.view(-1, 3, 224, 224))
+        # print(batch_y, outputs)
         # input()
         loss = loss_function(outputs, batch_y)
         loss.backward()
@@ -191,12 +232,12 @@ for epoch in range(EPOCHS):
     log.append([isample, osample, loss, dtm])
     if osample > valid_acc_min and epoch > 10:
         print('Acc increased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_acc_min, osample))
-        torch.save(net.state_dict(), "C:/Cache/PJF-30/intm_3.pt") #                                                  <-- UPDATE
+        torch.save(net.state_dict(), "C:/Cache/PJF-30/classes_lego_1_1.pt") #                                                  <-- UPDATE
         valid_acc_min = osample
 t1 = time.time()
 time_spend = t1-t0
+print("Time spend: ", time_spend)
 
-print("Time spend:", time_spend)
 train_data = np.array(train_data)
 isample = train_data[:, 0]
 osample = train_data[:, 1]
@@ -209,11 +250,14 @@ plt.xlabel("Epochs")
 plt.ylabel("Accuracy (in percentages)")
 plt.legend(["in-sample", "out-of-sample"], loc="lower right")
 plt.ylim([0, 1])
-plt.savefig(("intm_3.pdf")) #                                              <-- UPDATE
+plt.savefig(("classes_lego_1_1.pdf")) #                                              <-- UPDATE
 plt.show()
 
-# Max Out of Sample Accuracy: 0.914    3min 12s         6 - 192 - 64 - 2
-# Max Out of Sample Accuracy: 0.914    3min 11s         6 - 96 - 32 - 2
-# Max Out of Sample Accuracy: 0.914    3min 11s         6 - 48 - 16 - 2
-# Max Out of Sample Accuracy: 0.914    3min 9s          6 - 24 - 8 - 2
-# Max Out of Sample Accuracy: 0.914    2min 57s         6 - 12 - 4 - 2        <-- Selected
+# Conv: 12, 24  FC: 200, 100
+# Max Out of Sample Accuracy: 0.817    10min 46s (SGD, 0.01)
+
+# Conv: 12, 24  FC: 200, 100
+# Max Out of Sample Accuracy: 0.816    10min 44s (Adam, 0.001)  (1_1)   <-- Selected
+
+# Conv: 12, 24  FC: 200, 100
+# Max Out of Sample Accuracy: 0.835    10min 48s (Adam, 0.001; Dropout: 0.5)  (1)   <-- Selected
