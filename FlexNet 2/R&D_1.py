202b48bb423e5cb693ec0fb99fa87d8dc6b46080
Patzold <60754058+Patzold@users.noreply.github.com>
2021-01-05 22:06:39 +0100

    R&D and fix: net input size

--- FlexNet 2/R&D_1.py
+++ FlexNet 2/R&D_1.py
@@ -24,49 +24,6 @@ torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
-train_on_gpu = torch.cuda.is_available()
-if not train_on_gpu:
-    device = torch.device("cpu")
-    print('CUDA is not available.  Training on CPU ...')
-else:
-    device = torch.device("cuda:0")
-    print('CUDA is available!  Training on GPU ...')
-
-class Preconv(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, 2)
-        self.conv2 = nn.Conv2d(32, 32, 2)
-
-    def forward(self, x):
-        c1 = self.conv1(x)
-        print(c1.shape)
-        relu1 = F.relu(c1)
-        print(relu1.shape)
-        maxpool1 = F.max_pool2d(relu1, (2, 2))
-        return maxpool1
-preconv, Preconv()
-preconv.to(device)
-
-vgg16 = models.vgg16(pretrained=True)
-
-print("In features: ", vgg16.classifier[6].in_features)
-print("Out features: ", vgg16.classifier[6].out_features)
-
-# Freeze training for all "features" layers
-for param in vgg16.features.parameters():
-    param.requires_grad = False
-
-print(vgg16, preconv)
-
-layers = list(vgg16.features.children())[:-1]
-
-first_layer = layers[0]
-second_layer = layers[2]
-preconv.conv1 = first_layer
-preconv.conv2 = second_layer
-print(preconv)
-
 base_dir = "C:/Datasets/PJF-30/data/"
 save_dir = "C:/Datasets/PJF-30/safe/"
 nos = [4, 5, 6, 7] # Pig Head
@@ -75,7 +32,7 @@ yes = [1, 2, 3]
 train = []
 test = []
 
-if True:
+if False:
     out_train = []
     out_test = []
     for indx, dir in tqdm(enumerate(nos)):
@@ -84,14 +41,10 @@ if True:
             try:
                 img_in = cv2.imread((path + "/" + img), cv2.IMREAD_COLOR)
                 img_resz = cv2.resize(img_in, (224, 224))
-                img_tnsr = torch.from_numpy(img_resz).to(torch.float32).to(device)
-                net_out = preconv(img_tensor.view(-1, 3, 224, 224))
                 if num < 2000:
-                    out_train.append([net_out.cpu(), 0])
-                    print(net_out.cpu(), net_out.cpu().shape())
-                    input()
+                    out_train.append([img_resz, 0])
                 else:
-                    out_test.append([net_out.cpu(), 0])
+                    out_test.append([img_resz, 0])
             except Exception as e: pass
     random.shuffle(out_train)
     random.shuffle(out_test)
@@ -106,12 +59,10 @@ if True:
             try:
                 img_in = cv2.imread((path + "/" + img), cv2.IMREAD_COLOR)
                 img_resz = cv2.resize(img_in, (224, 224))
-                img_tnsr = torch.from_numpy(img_resz).to(torch.float32).to(device)
-                net_out = preconv(img_tensor.view(-1, 3, 224, 224))
                 if num < 2000:
-                    out_train.append([net_out.cpu(), 1])
+                    out_train.append([img_resz, 1])
                 else:
-                    out_test.append([net_out.cpu(), 1])
+                    out_test.append([img_resz, 1])
             except Exception as e: pass
     random.shuffle(out_train)
     random.shuffle(out_test)
@@ -121,16 +72,16 @@ if True:
     print(len(train), len(test))
 
     # train = np.array(train)
-    pickle_out = open((save_dir + "rnd_cat_rubt_1.pickle"),"wb")
+    pickle_out = open((save_dir + "categorys_rubt_1.pickle"),"wb")
     pickle.dump(train, pickle_out)
     pickle_out.close()
-    pickle_out = open((save_dir + "rnd_cat_rubt_1t.pickle"),"wb")
+    pickle_out = open((save_dir + "categorys_rubt_1t.pickle"),"wb")
     pickle.dump(test, pickle_out)
     pickle_out.close()
 else:
-    pickle_in = open(save_dir + "rnd_cat_rubt_1.pickle","rb")
+    pickle_in = open(save_dir + "categorys_rubt_1.pickle","rb")
     train = pickle.load(pickle_in)
-    pickle_in = open(save_dir + "rnd_cat_rubt_1t.pickle","rb")
+    pickle_in = open(save_dir + "categorys_rubt_1t.pickle","rb")
     test = pickle.load(pickle_in)
 l = len(train)
 lt = len(test)
@@ -174,3 +125,189 @@ check = [0, 0, 0]
 for i in range(lt):
         check[yt[i].numpy()] += 1
 print(check)
+
+train_on_gpu = torch.cuda.is_available()
+if not train_on_gpu:
+    device = torch.device("cpu")
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    device = torch.device("cuda:0")
+    print('CUDA is available!  Training on GPU ...')
+
+class Preconv(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 2)
+        self.conv2 = nn.Conv2d(32, 32, 2)
+
+    def forward(self, x):
+        c1 = self.conv1(x)
+        relu1 = F.relu(c1)
+        c2 = self.conv2(relu1)
+        relu2 = F.relu(c2)
+        maxpool1 = F.max_pool2d(relu2, (2, 2))
+        return maxpool1
+preconv = Preconv()
+
+vgg16 = models.vgg16(pretrained=True)
+
+print("In features: ", vgg16.classifier[6].in_features)
+print("Out features: ", vgg16.classifier[6].out_features)
+
+# Freeze training for all "features" layers
+for param in vgg16.features.parameters():
+    param.requires_grad = False
+
+print(vgg16, preconv)
+
+layers = list(vgg16.features.children())[:-1]
+
+first_layer = layers[0]
+second_layer = layers[2]
+preconv.conv1 = first_layer
+preconv.conv2 = second_layer
+print(preconv)
+preconv.to(device)
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(64, 64, 2)
+        self.conv2 = nn.Conv2d(64, 128, 2)
+        self.dropout = nn.Dropout(0.75)
+        
+        x = torch.randn(112,112,64).view(-1,64,112,112)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 300) #flattening.
+        self.fc2 = nn.Linear(300, 100)
+        self.fc3 = nn.Linear(100, 2)
+
+    def convs(self, x):
+            c1 = self.conv1(x)
+            relu1 = F.relu(c1)
+            pool1 = F.max_pool2d(relu1, (2, 2))
+            c2 = self.conv2(pool1)
+            relu2 = F.relu(c2)
+            pool2 = F.max_pool2d(relu2, (2, 2))
+            
+            if self._to_linear is None:
+                self._to_linear = pool2[0].shape[0]*pool2[0].shape[1]*pool2[0].shape[2]
+                print("to linear: ", self._to_linear)
+            return pool2
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+net.to(device)
+print(net)
+
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+loss_function = nn.CrossEntropyLoss()
+
+BATCH_SIZE = 100
+EPOCHS = 50
+
+train_log = []
+eval_size = int(len(X)*0.1)
+eval_X = X[:eval_size]
+eval_y = y[:eval_size]
+print("After eval split: ", X.shape, y.shape)
+
+train_data = []
+log = []
+valid_loss_min = np.Inf # track change in validation loss
+valid_acc_min = 0
+
+def evaluate():
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i in tqdm(range(len(eval_X))):
+            real_class = eval_y[i].to(device)
+            preconv_out = preconv(eval_X.view(-1, 3, 112, 112))
+            net_out = net(preconv_out.view(-1, 64, 112, 112))
+            predicted_class = torch.argmax(net_out)
+            if predicted_class == real_class:
+                correct += 1
+            # else: cv2.imwrite(("D:/Datasets\stupid/test/o" + str(i) + ".jpg"), eval_X[i].view(75, 75, 1).numpy())
+            total += 1
+    in_sample_acc = round(correct/total, 3)
+    correct = 0
+    total = 0
+    # Xta = Xt[:1500]
+    # yta = yt[:1500]
+    check = [0, 0, 0, 0, 0, 0]
+    with torch.no_grad():
+        for i in tqdm(range(len(Xt))):
+            real_class = yt[i].to(device)
+            preconv_out = preconv(Xt.view(-1, 3, 112, 112))
+            net_out = net(preconv_out.view(-1, 64, 112, 112))
+            predicted_class = torch.argmax(net_out)
+            if predicted_class == real_class:
+                correct += 1
+                check[predicted_class.cpu().numpy()] += 1
+            # else: cv2.imwrite(("D:/Datasets\stupid/test/i" + str(i) + ".jpg"), Xt[i].view(60, 60, 1).numpy())
+            total += 1
+    print(check)
+    out_of_sample_acc = round(correct/total, 3)
+    return in_sample_acc, out_of_sample_acc
+
+t0 = time.time()
+for epoch in range(EPOCHS):
+    dtm = str(datetime.datetime.now())
+    for i in tqdm(range(0, len(X), BATCH_SIZE)): # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
+        net.train()
+        # try:
+        batch_X = X[i:i+BATCH_SIZE]
+        batch_y = y[i:i+BATCH_SIZE]
+        batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+        batch_data = []
+
+        # Actual training
+        net.zero_grad()
+        optimizer.zero_grad()
+        preconv_out = preconv(batch_X.view(-1, 3, 224, 224))
+        print(preconv_out.shape)
+        outputs = net(preconv_out.view(100, 64, 112, 112))
+        print(outputs.shape, batch_y.shape)
+        loss = loss_function(outputs, batch_y)
+        input()
+        loss.backward()
+        optimizer.step() # Does the update
+
+    print(f"Epoch: {epoch}. Loss: {loss}")
+    isample, osample = evaluate()
+    print("In-sample accuracy: ", isample, "  Out-of-sample accuracy: ", osample)
+    train_data.append([isample, osample])
+    log.append([isample, osample, loss, dtm])
+    if osample > valid_acc_min and epoch > 10:
+        print('Acc increased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_acc_min, osample))
+        # torch.save(net.state_dict(), "C:/Cache/PJF-30/categorys_rubt_1_1.pt") #                                                  <-- UPDATE
+        valid_acc_min = osample
+t1 = time.time()
+time_spend = t1-t0
+
+print("Time spend:", time_spend)
+train_data = np.array(train_data)
+isample = train_data[:, 0]
+osample = train_data[:, 1]
+
+plt.plot(isample)
+plt.plot(osample)
+plt.title("Model evaluation results")
+plt.grid()
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy (in percentages)")
+plt.legend(["in-sample", "out-of-sample"], loc="lower right")
+plt.ylim([0, 1])
+plt.savefig(("rubt_1.pdf")) #                                              <-- UPDATE
+plt.show()
