9b12e2a6f8b8b380838248c7b62062eb24d966a3
Patzold <60754058+Patzold@users.noreply.github.com>
2021-01-10 02:10:12 +0100

    FlexNet 1 implementation 2

--- FlexNet 1 implementation 2/category_submodels.py
+++ FlexNet 1 implementation 2/category_submodels.py
@@ -24,6 +24,17 @@ torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
+base_dir = "C:/Datasets/PJF-30/data/"
+save_dir = "C:/Datasets/PJF-30/safe/"
+
+pickle_in = open(save_dir + "img_tensor.pickle","rb")
+train = pickle.load(pickle_in)
+pickle_in = open(save_dir + "img_tensor_test.pickle","rb")
+test = pickle.load(pickle_in)
+
+X, y = train
+Xt, yt = test
+
 train_on_gpu = torch.cuda.is_available()
 if not train_on_gpu:
     device = torch.device("cpu")
@@ -68,7 +79,6 @@ class RubberToy(nn.Module):
         x = self.fc3(x)
         return x
 
-
 class PigHead(nn.Module):
     def __init__(self):
         super().__init__()
@@ -105,13 +115,95 @@ class PigHead(nn.Module):
         x = self.fc3(x)
         return x
 
-rubt, pig = RubberToy(), PigHead()
+class Lego(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 2)
+        self.conv2 = nn.Conv2d(32, 64, 2)
+        self.dropout = nn.Dropout(0.75)
+        
+        x = torch.randn(224,224,3).view(-1,3,224,224)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 300) #flattening.
+        self.fc2 = nn.Linear(300, 100)
+        self.fc3 = nn.Linear(100, 2)
+
+    def convs(self, x):
+            c1 = self.conv1(x)
+            relu1 = F.relu(c1)
+            pool1 = F.max_pool2d(relu1, (2, 2))
+            c2 = self.conv2(pool1)
+            relu2 = F.relu(c2)
+            pool2 = F.max_pool2d(relu2, (2, 2))
+            
+            if self._to_linear is None:
+                self._to_linear = pool2[0].shape[0]*pool2[0].shape[1]*pool2[0].shape[2]
+                print("to linear: ", self._to_linear)
+            return pool2
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+class Can(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 2)
+        self.conv2 = nn.Conv2d(32, 64, 2)
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        self.dropout = nn.Dropout(0.8)
+        
+        x = torch.randn(224,224,3).view(-1,3,224,224)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 700) #flattening.
+        self.fc2 = nn.Linear(700, 100)
+        self.fc3 = nn.Linear(100, 2)
+
+    def convs(self, x):
+            x = self.conv1(x)
+            x = F.relu(x)
+            x = F.max_pool2d(x, (2, 2))
+            x = self.conv2(x)
+            x = F.relu(x)
+            x = F.max_pool2d(x, (2, 2))
+            x = self.conv3(x)
+            x = F.relu(x)
+            x = F.max_pool2d(x, (2, 2))
+            
+            if self._to_linear is None:
+                self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+                print("to linear: ", self._to_linear)
+            return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+rubt, pig, lego, can = RubberToy(), PigHead(), Lego(), Can()
 rubt.load_state_dict(torch.load("C:/Cache/PJF-30/categorys_rubt_1_1.pt"))
 pig.load_state_dict(torch.load("C:/Cache/PJF-30/categorys_pig_1.pt"))
+lego.load_state_dict(torch.load("C:/Cache/PJF-30/categorys_lego_2.pt"))
+can.load_state_dict(torch.load("C:/Cache/PJF-30/categorys_can_1.pt"))
 rubt.to(device)
 pig.to(device)
+lego.to(device)
+can.to(device)
 rubt.eval()
 pig.eval()
+lego.eval()
+can.eval()
 
 # v1: raw net output
 # v2: argmax
@@ -127,3 +219,22 @@ def run(input_tensor):
         # out = [rubt_argmax, pig_argmax]  # v2
         # out = rubt_out + pig_out  # v1
         return out
+
+intm = []
+for i in tqdm(range(len(y))):
+    result = run(X[i].view(-1, 3, 224, 224).to(device))
+    intm.append(result)
+pickle_out = open((save_dir + "intm.pickle"),"wb")
+pickle.dump(intm, pickle_out)
+pickle_out.close()
+
+intm = []
+for i in tqdm(range(len(yt))):
+    result = run(Xt[i].view(-1, 3, 224, 224).to(device))
+    intm.append(result)
+pickle_out = open((save_dir + "intm_t.pickle"),"wb")
+pickle.dump(intm, pickle_out)
+pickle_out.close()
+
+# Train part: 2m 57s
+# Test part: 44s
---
+++ FlexNet 1 implementation 2/intm_layer.py
@@ -0,0 +1,192 @@
+import os
+os.chdir("FlexNet 1 implementation 2")
+import random
+import matplotlib.pyplot as plt
+import datetime
+import time
+import cv2
+import pickle
+from tqdm import tqdm
+import numpy as np
+
+import torch
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# For reproducibility
+seed = 3
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+
+base_dir = "C:/Datasets/PJF-30/data/"
+save_dir = "C:/Datasets/PJF-30/safe/"
+
+pickle_in = open(save_dir + "intm.pickle","rb")
+X = pickle.load(pickle_in)
+pickle_in = open(save_dir + "intm_t.pickle","rb")
+Xt = pickle.load(pickle_in)
+
+pickle_in = open(save_dir + "img_tensor.pickle","rb")
+train = pickle.load(pickle_in)
+pickle_in = open(save_dir + "img_tensor_test.pickle","rb")
+test = pickle.load(pickle_in)
+i, y = train
+it, yt = test
+
+X = torch.Tensor(X)
+Xt = torch.Tensor(Xt)
+X.to(torch.float32)
+Xt.to(torch.int64)
+print(X.dtype)
+print(y[10:], yt[:10])
+print(X[10:], Xt[:10])
+
+check = [0, 0, 0, 0]
+for i in range(l):
+        check[y[i].numpy()] += 1
+print(check)
+check = [0, 0, 0, 0]
+for i in range(lt):
+        check[yt[i].numpy()] += 1
+print(check)
+
+train_on_gpu = torch.cuda.is_available()
+theCPU = torch.device("cpu")
+
+if not train_on_gpu:
+    device = torch.device("cpu")
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    device = torch.device("cuda:0")
+    print('CUDA is available!  Training on GPU ...')
+
+# THE NETWORK
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(12, 24)
+        self.fc2 = nn.Linear(24, 8)
+        self.fc3 = nn.Linear(8, 4)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+net = Net()
+net.to(device)
+print(net)
+
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
+loss_function = nn.CrossEntropyLoss()
+
+BATCH_SIZE = 100
+EPOCHS = 100
+
+train_log = []
+eval_size = int(len(X)*0.1)
+eval_X = X[:eval_size]
+eval_y = y[:eval_size]
+print("After eval split: ", X.shape, y.shape)
+
+train_data = []
+log = []
+valid_loss_min = np.Inf # track change in validation loss
+valid_acc_min = 0
+
+def evaluate():
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i in tqdm(range(len(eval_X))):
+            real_class = eval_y[i].to(device)
+            net_out = net(eval_X[i].view(-1, 12).to(device))[0]  # returns a list
+            predicted_class = torch.argmax(net_out)
+            # print(real_class, net_out, predicted_class)
+            # input()
+            if predicted_class == real_class:
+                correct += 1
+            # else: cv2.imwrite(("D:/Datasets\stupid/test/o" + str(i) + ".jpg"), eval_X[i].view(75, 75, 1).numpy())
+            total += 1
+    in_sample_acc = round(correct/total, 3)
+    correct = 0
+    total = 0
+    # Xta = Xt[:1500]
+    # yta = yt[:1500]
+    check = [0, 0, 0, 0]
+    with torch.no_grad():
+        for i in tqdm(range(len(Xt))):
+            real_class = yt[i].to(device)
+            net_out = net(Xt[i].view(-1, 12).to(device))[0]  # returns a list
+            predicted_class = torch.argmax(net_out)
+            if predicted_class == real_class:
+                correct += 1
+                check[predicted_class.cpu().numpy()] += 1
+            # else: cv2.imwrite(("D:/Datasets\stupid/test/i" + str(i) + ".jpg"), Xt[i].view(60, 60, 1).numpy())
+            total += 1
+    out_of_sample_acc = round(correct/total, 3)
+    print(check)
+    return in_sample_acc, out_of_sample_acc
+
+t0 = time.time()
+for epoch in range(EPOCHS):
+    dtm = str(datetime.datetime.now())
+    for i in tqdm(range(0, len(X), BATCH_SIZE)): # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
+        net.train()
+        # try:
+        batch_X = X[i:i+BATCH_SIZE]
+        batch_y = y[i:i+BATCH_SIZE]
+        batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+        batch_data = []
+
+        # Actual training
+        net.zero_grad()
+        optimizer.zero_grad()
+        outputs = net(batch_X.view(-1, 12))
+        # print(batch_X, batch_y, outputs)
+        # input()
+        loss = loss_function(outputs, batch_y)
+        loss.backward()
+        optimizer.step() # Does the update
+
+    print(f"Epoch: {epoch}. Loss: {loss}")
+    isample, osample = evaluate()
+    print("In-sample accuracy: ", isample, "  Out-of-sample accuracy: ", osample)
+    train_data.append([isample, osample])
+    log.append([isample, osample, loss, dtm])
+    if osample > valid_acc_min and epoch > 10:
+        print('Acc increased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_acc_min, osample))
+        torch.save(net.state_dict(), "C:/Cache/PJF-30/intm.pt") #                                                  <-- UPDATE
+        valid_acc_min = osample
+t1 = time.time()
+time_spend = t1-t0
+
+print("Time spend:", time_spend)
+train_data = np.array(train_data)
+isample = train_data[:, 0]
+osample = train_data[:, 1]
+
+plt.plot(isample)
+plt.plot(osample)
+plt.title("Model evaluation results")
+plt.grid()
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy (in percentages)")
+plt.legend(["in-sample", "out-of-sample"], loc="lower right")
+plt.ylim([0, 1])
+plt.savefig(("intm.pdf")) #                                              <-- UPDATE
+plt.show()
+
+ # Max Out of Sample Accuracy: 0.914    3min 12s         9 - 192 - 64 - 3
+# Max Out of Sample Accuracy: 0.782    5min 14s         12 - 96 - 32 - 4
+# Max Out of Sample Accuracy: 0.782    5min 7s         12 - 48 - 16 - 4
+# Max Out of Sample Accuracy: 0.783    5min 3s         12 - 24 - 8 - 4        <-- Selected
+ # Max Out of Sample Accuracy: 0.869    4min 50s         9 - 12 - 4 - 3
